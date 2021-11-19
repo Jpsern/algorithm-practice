feb223e7ab3c382d5b56501aad7d2b774560d960
jpsern <tommy999_jps@yahoo.co.jp>
2021-11-20 02:25:27 +0900

    とりあえず簡単なFizzBuzzを作ってみる
    計算量的には無駄がひとつ多いけど、とりあえず動かしてみるだけなので
    いまはあまり細かいところにはこだわらない。

---
+++ app/tutorial.py
@@ -0,0 +1,11 @@
+class Tutorial:
+    def fizzbuzz(value: int):
+        if value % 15 == 0:
+            return 'FizzBuzz'
+        if value % 5 == 0:
+            return 'Buzz'
+        if value % 3 == 0:
+            return 'Fizz'
+        else:
+            return value
+
