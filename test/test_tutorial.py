4d2aa616563354b3dab3c4a2e575736bb0ff8954
TOMMY a.k.a JPS <tommy999_jps@yahoo.co.jp>
2021-11-20 17:33:16 +0900

    FizzBuzzのテストをちゃんとやる (#5)
    * wip
    
    * .gitkeepはもういらないので削除
    
    * テストのパターンを増やした
    
    * テスト実行トリガーからブランチの制限を撤廃
    
    * テストもコケることを確認したので値を修正
    
    * Update README.md [skip ci]

--- test/test_tutorial.py
+++ test/test_tutorial.py
@@ -2,6 +2,18 @@ import unittest
 from src.tutorial import Tutorial
 
 class TestTutorial(unittest.TestCase):
-    def test_fizzbuzz(self):
-        self.assertEqual(1, Tutorial.fizzbuzz(1))
+    def test_fizzbuzz_normal(self):
+        for num in [1, 2, 4, 7, 8, 11, 13, 14, 16]:
+            self.assertEqual(Tutorial.fizzbuzz(num), num, '渡した値をそのまま返す')
 
+    def test_fizzbuzz_multiple_three(self):
+        for num in [3, 6, 9, 12]:
+            self.assertEqual(Tutorial.fizzbuzz(num), 'Fizz', 'Fizzを返すべき')
+
+    def test_fizzbuzz_multiple_five(self):
+        for num in [5, 10, 20, 25]:
+            self.assertEqual(Tutorial.fizzbuzz(num), 'Buzz', 'Buzzを返すべき')
+
+    def test_fizzbuzz_multiple_fifteen(self):
+        for num in [15, 30, 45, 60]:
+            self.assertEqual(Tutorial.fizzbuzz(num), 'FizzBuzz', 'FizzBuzzを返すべき')
