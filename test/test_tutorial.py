89cfd67d5ce040e869bd732f5d7580aa60395ef1
Jpsern <tommy999_jps@yahoo.co.jp>
2021-11-23 02:42:05 +0900

    メッセージ修正

--- test/test_tutorial.py
+++ test/test_tutorial.py
@@ -8,15 +8,15 @@ class TestTutorial(unittest.TestCase):
 
     def test_fizzbuzz_multiple_three(self):
         for num in [3, 6, 9, 12]:
-            self.assertEqual(Tutorial.fizzbuzz(num), 'Fizz', 'Fizzを返すべき')
+            self.assertEqual(Tutorial.fizzbuzz(num), 'Fizz', '3の倍数はFizzを返す')
 
     def test_fizzbuzz_multiple_five(self):
         for num in [5, 10, 20, 25]:
-            self.assertEqual(Tutorial.fizzbuzz(num), 'Buzz', 'Buzzを返すべき')
+            self.assertEqual(Tutorial.fizzbuzz(num), 'Buzz', '5の倍数はBuzzを返す')
 
     def test_fizzbuzz_multiple_fifteen(self):
         for num in [15, 30, 45, 60]:
-            self.assertEqual(Tutorial.fizzbuzz(num), 'FizzBuzz', 'FizzBuzzを返すべき')
+            self.assertEqual(Tutorial.fizzbuzz(num), 'FizzBuzz', '15の倍数はFizzBuzzを返す')
     
     def test_is_prime_number(self):
         self.assertListEqual(
